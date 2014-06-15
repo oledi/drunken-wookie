48e21596cdf4277e5c171daa4678a317284ccb4b
Floyd van Boksel <floyd.van.boksel@gmail.com>
2014-06-15 22:09:56 +0200

    Added start and update
    Basic start and update function that check the accelerometer for changes

--- www/static/js/handlers/exercise.js
+++ www/static/js/handlers/exercise.js
@@ -1,8 +1,32 @@
-function Exercise() {
+function Exercise(accelerometer) {
+	this.exerciseInterval = {};
 	this.position = [];
 	this.breakpoints = [];
+	this.accelerometer = accelerometer;
+
+	this.start();
 }
 
-Exercise.prototype.start() = function(){
-	
+Exercise.prototype.start = function() {
+	var self = this;
+
+	self.exerciseInterval.excercise = setInterval(function() {
+		console.log(self.accelerometer.breakpoint);
+	}, 100);
+}
+
+Exercise.prototype.checkForBreak = function() {
+	var self = this;
+
+	self.exerciseInterval.update = setInterval(function() {
+		if(self.accelerometer.breakpoint) {
+					
+
+			self.accelerometer.breakpoint = false;
+		}
+	}, 500);
+}
+
+Exercise.prototype.end = function() {
+
 }
