48dabd4599051a25dc53a3127d9328485fa85ab9
Floyd van Boksel <floyd.van.boksel@gmail.com>
2014-06-20 21:29:51 +0200

    Added buttons + removed old ones

--- www/floyd-fabian/static/js/app.js
+++ www/floyd-fabian/static/js/app.js
@@ -60,21 +60,40 @@
         		storage.cleanLocalstorage();
         	});
 
-        	$('.btn_start').click(function(){
+        	// Page 3 is excersises
+        	// Page 4 is recording
+        	$('#page3 .btn_start').click(function(){
 			   	$('.btn_start').toggle();
 				$('.btn_stop').toggle();
 
 				exercise.startRecord();
 			});
 
-			$('.btn_stop').click(function(){
+			$('#page4 .btn_start').click(function(){
+			   	$('.btn_start').toggle();
+				$('.btn_stop').toggle();
+
+				exercise.startRecord();
+			});
+
+			$('#page3 .btn_stop').click(function(){
+			   	$('.btn_start').toggle();
+				$('.btn_stop').toggle();
+			
+				exercise.saveRecord();
+
+				// $('.pages').hide();
+				// $('#page2').show();
+			});
+
+			$('#page4 .btn_stop').click(function(){
 			   	$('.btn_start').toggle();
 				$('.btn_stop').toggle();
 				
 				exercise.saveRecord();
-			
+
 				// $('.pages').hide();
-				// $('#page2').show();
+				// $('#page1').show();
 			});
 
 			$('#page1').show();
@@ -109,26 +128,6 @@
 				$('#page3').show();
 					 
 			    return false;
-			});
-
-			$('#page3 .btn_stop').click(function(){
-			   	$('.btn_start').toggle();
-				$('.btn_stop').toggle();
-					 
-				$('.pages').hide();
-				$('#page2').show();
-				
-				stop();
-			});
-
-			$('#page4 .btn_stop').click(function(){
-			   	$('.btn_start').toggle();
-				$('.btn_stop').toggle();
-					 
-				$('.pages').hide();
-				$('#page1').show();
-				
-				stop();
 			});
 
 			$('#page2 #bgcontainer_menu').click(function(){
